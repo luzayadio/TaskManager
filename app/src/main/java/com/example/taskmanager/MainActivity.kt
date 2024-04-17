1080bcb296b93d21c5ecd10954d09e21337ea109
Luzayadio Vila <71708339+luzayadio@users.noreply.github.com>
2024-04-17 23:13:27 +0100

    Finished Code

---
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,214 @@
+package com.example.taskmanager
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //FirstScreen()
+                    //SecondScreen(modifier = Modifier.fillMaxSize())
+                    ThirdScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FirstScreen(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+        )
+        Text(
+            text = "Jetpack Compose tutorial",
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp,
+
+            )
+        Text(
+            text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify,
+        )
+        Text(
+            text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify,
+        )
+    }
+}
+
+@Composable
+fun SecondScreen(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_task_completed)
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+        )
+        Text(
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp,
+        )
+    }
+}
+
+@Composable
+fun ThirdScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Row(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFEADDFF)),
+                verticalArrangement = Arrangement.Center,
+                ) {
+                Column(modifier = modifier.padding(16.dp),
+                     horizontalAlignment = Alignment.CenterHorizontally,
+                     ) {
+                     Text(
+                         text = "Text composable",
+                         fontWeight = FontWeight.Bold,
+                         modifier = Modifier.padding(bottom = 16.dp),
+                     )
+                     Text(
+                         text = "Displays text and follows the recommended Material Design guidelines.",
+                         textAlign = TextAlign.Justify,
+                         )
+                 }
+            }
+            Column(
+                modifier = modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFD0BCFF)),
+                verticalArrangement = Arrangement.Center,
+                ) {
+                Column(modifier = modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                    Text(
+                        text = "Image composable",
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(bottom = 16.dp),
+                    )
+                    Text(
+                        text = "Creates a composable that lays out and draws a given Painter class object.",
+                        textAlign = TextAlign.Justify,
+                        )
+                }
+
+            }
+        }
+        Row(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFB69DF8)),
+                verticalArrangement = Arrangement.Center,
+                ) {
+                Column(modifier = modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,) {
+                    Text(
+                        text = "Row composable",
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(bottom = 16.dp),
+                    )
+                    Text(
+                        text = "A layout composable that places its children in a horizontal sequence.",
+                        textAlign = TextAlign.Justify,
+                        )
+                }
+
+            }
+            Column(
+                modifier = modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFF6EDFF)),
+                verticalArrangement = Arrangement.Center,
+                ) {
+                Column(modifier = modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                    Text(
+                        text = "Column composable",
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(bottom = 16.dp),
+                    )
+                    Text(
+                        text = "A layout composable that places its children in a vertical sequence.",
+                        textAlign = TextAlign.Justify,
+                        )
+                }
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TaskManagerTheme {
+        //FirstScreen()
+        //SecondScreen(modifier = Modifier.fillMaxSize())
+        ThirdScreen()
+    }
+}
